787f9baf7f0a1d040c92c2b50169cadeeeb3238f
System Administrator <root@BopengdeMacBook-Pro.local>
2018-04-28 10:43:32 -0400

    Add test cases

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+import (
+  "testing"
+  "flag"
+)
+
+func printInfo() {
+    fmt.Println("Hello World")
+}
+
+func main() {
+    printInfo() 
+}
+
+var systemTest *bool
+func init() {
+  systemTest = flag.Bool("systemTest", false, "Set to true when running system tests")
+}
+
+func TestSystem(t *testing.T) {
+  if *systemTest {
+     main()
+  }
+}
+
